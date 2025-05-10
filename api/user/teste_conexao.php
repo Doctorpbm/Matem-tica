410441c9db0950d5e1cdc795feb3c64ab8aa152a
Doctorpbm <caio123pbm@gmail.com>
2025-05-10 19:15:52 -0300

    Add files via upload

---
+++ api/user/teste_conexao.php
@@ -0,0 +1,17 @@
+<?php
+
+
+$servername = "database-2.cdku2ia0sqoy.us-east-2.rds.amazonaws.com";
+$username = "admin";
+$password = "xrl8melhoralien";
+$dbname = "questionsmath";
+
+// Cria a conexão
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verifica a conexão
+if ($conn->connect_error) {
+    die("Falha na conexão: " . $conn->connect_error);
+}
+echo "Conexão bem-sucedida!";
+?>
